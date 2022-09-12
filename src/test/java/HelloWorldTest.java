4a38295ae5059c5be2f1d47505a30b62f62c4d9b
Pletmentsev <apletmentsev@gmail.com>
2022-09-12 15:50:18 +0300

    FirstGetHelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
+
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello World!");
+    public void testHelloWorld() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        System.out.println(response.asString());
     }
 }
