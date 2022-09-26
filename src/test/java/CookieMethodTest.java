156bef681d66b5718a3f7aeab3994aeaa3b1b049
Pletmentsev <apletmentsev@gmail.com>
2022-09-26 16:36:05 +0300

    CookieMethodTest

---
+++ src/test/java/CookieMethodTest.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CookieMethodTest {
+    @Test
+    public void testStringLength() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        assertTrue (response.getHeaders().hasHeaderWithName("Set-Cookie"), "Response doesn't have 'Set-Cookie' headers");
+        assertEquals ("hw_value", response.getCookie("HomeWork"), "Cookies not equal expected result");
+    }
+}
